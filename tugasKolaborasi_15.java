da25e84559c7970432268320db8a55615a463c09
Shan0n73 <agrisyandimaraja@gmail.com>
2025-10-01 13:11:44 +0700

    Balasan

--- tugasKolaborasi_15.java
+++ tugasKolaborasi_15.java
@@ -1,5 +1,7 @@
 public class tugasKolaborasi_15 {
     public static void main(String[] args) {
         System.out.println("hallo agrii");
+        System.out.println("hlo juga, gofar");
+        System.out.println("apa kabar?");
     }
 }
